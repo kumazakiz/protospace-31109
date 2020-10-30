116dfb69f6bf81a229ff759e6b1690d442675980
kumazakiz <tenka2dsk@gmail.com>
2020-10-30 20:33:51 +0900

    エラーの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,8 +33,8 @@ class PrototypesController < ApplicationController
 
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
       render :edit
